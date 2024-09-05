b99695e2aadc9645aa5cfdd301f4c090b981e071
Isabelle Perez <isabelle.perez@uconn.edu>
2024-09-05 17:26:28 -0400

    Decrypt message two

--- decrypt_message_two.py
+++ decrypt_message_two.py
@@ -5,3 +5,20 @@ encrypted_message = encrypted_file.readline()
 encrypted_file.close()
 
 # Write Code Here
+
+# initialize start and end indices
+start_idx = 1
+end_idx = len(encrypted_message) - 2
+
+# split message into list of characters
+message = [char for char in encrypted_message]
+
+# swap characters
+while start_idx < end_idx: 
+    message[start_idx], message[end_idx] = message[end_idx], message[start_idx]
+    start_idx += 2
+    end_idx -= 2
+
+decrypted_message = ''.join(message)
+
+print(decrypted_message)
